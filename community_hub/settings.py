a13cab13265eb9977454e679e6a3c627b04d7d4b
Conner-Houser <connerhouser@mail.weber.edu>
2025-12-03 13:11:26 -0700

    collect static

--- community_hub/settings.py
+++ community_hub/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
